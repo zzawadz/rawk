9efa76cb992739aec7b12427b95bd32235554ce8
Zygmunt Zawadzki <zawadzkizygmunt@gmail.com>
2016-02-19 00:05:41 +0100

    Save awk scripts in pkg functions

--- R/rawk.R
+++ R/rawk.R
@@ -5,18 +5,32 @@ rawk_find_awk = function()
 
 rawk = function(code = '{print $1}')
 {
-  call = paste(sprintf(" -e '%s'", code), " %s")
+  callCode = paste(sprintf(" -e '%s'", code), " %s")
 
   function(file, stdout = "", ...)
   {
     fileName = tools::file_path_as_absolute(file)
-    args = sprintf(call, fileName)
+    args = sprintf(callCode, fileName)
+    system2(rawk_find_awk(), args = args, stdout = stdout, ...)
+  }
+}
+
+rawk_pkg = function(script, pkgName)
+{
+  path = file.path(find.package("rawk"), "awk", script)
+
+  callCode = paste(sprintf(" -f '%s'", path), " %s")
+
+  function(file, stdout = "", ...)
+  {
+    fileName = tools::file_path_as_absolute(file)
+    args = sprintf(callCode, fileName)
     system2(rawk_find_awk(), args = args, stdout = stdout, ...)
   }
 }
 
 rawk_first_column = rawk()
-
+rawk_pkg_test = rawk_pkg("awkTest.awk", "rawk")
 
 #a = system(path, intern = TRUE)
 #a
