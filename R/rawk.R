e338dd96271d40f8d399c84394aa15b8e15afcef
Zygmunt Zawadzki <zawadzkizygmunt@gmail.com>
2016-04-02 13:19:25 +0200

    Manual for rawk

--- R/rawk.R
+++ R/rawk.R
@@ -1,3 +1,33 @@
+
+#' Create awk function.
+#'
+#'
+#'
+#' @param code awk code
+#'
+#' @export
+#'
+#' @return Retrun function which executes awk code on a file.
+#'
+#' Returned function arguments:
+#'
+#' \code{file} path to file
+#'
+#' \code{stdout} standard output. By default result is printed into console. See \link{system2} for futher description.
+#'
+#' \code{...} Other arguments passed to \link{system2}.
+#'
+#' @examples
+#'
+#' file = tempfile()
+#' cat('1\n2\n', file = file)
+#'
+#' rawk()(file)
+#'
+#' # Capture output
+#' awk = rawk('{print $0}')
+#' awk(file, stdout = TRUE)
+#'
 rawk = function(code = '{print $1}')
 {
   callCode = paste(sprintf(" -e '%s'", code), " %s")
@@ -9,6 +39,7 @@ rawk = function(code = '{print $1}')
     system2(rawk_get_awk_path(), args = args, stdout = stdout, ...)
   }
 }
+
 
 rawk_pkg = function(script, pkgName)
 {
@@ -26,14 +57,5 @@ rawk_pkg = function(script, pkgName)
 
 rawk_first_column = rawk()
 rawk_pkg_test = rawk_pkg("awkTest.awk", "rawk")
-
-#a = system(path, intern = TRUE)
-#a
-#tmp = tools::file_path_as_absolute("tmp")
-#file = "R/rawk.R"
-#
-#system(sprintf(call, fileName), intern = TRUE)
-#rawk_first_column("DESCRIPTION")
-
 
 
