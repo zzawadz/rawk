1644eee7282b6588ee18a9e48b208fad2d1fee5a
zzawadzki <zawadzkizygmunt@gmail.com>
2016-04-03 13:44:15 +0200

    Caching tests

--- tests/testthat/test-file_function.R
+++ tests/testthat/test-file_function.R
@@ -19,3 +19,21 @@ test_that("Arguments order",
   expect_equal(fnc(.CACHE_VERBOSE = TRUE, tmpFile), "5")
 })
 
+test_that("Caching",
+{
+  set.seed(123)
+
+  tmpFile = tempfile()
+  cat("5\n", file = tmpFile)
+
+  file_fnc = function(x)
+  {
+    n = as.numeric(readLines(x))
+    rnorm(n)
+  }
+
+  fnc = file_modification_time_cache(file_fnc)
+  expect_equal(fnc(tmpFile, .CACHE_VERBOSE = FALSE), fnc(.CACHE_VERBOSE = TRUE, tmpFile))
+})
+
+
